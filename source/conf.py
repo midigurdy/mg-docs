b5d7c772b2d9a3cb0a64edaf3dc4e514a054ee3a
Marcus Weseloh <marcus@weseloh.cc>
2019-05-02 21:11:27 +0200

    Include rST sources for search results

--- source/conf.py
+++ source/conf.py
@@ -61,7 +61,9 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_copy_source = False
+html_copy_source = True
+
+html_show_sourcelink = False
 
 html_theme_options = {
 #    'canonical_url': '',
